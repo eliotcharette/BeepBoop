7758f31c74a49900024e1479a39a043546fc4982
eliotcharette <eliotcharette@gmail.com>
2018-06-08 11:42:52 -0700

    Clean code

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,19 @@ var beepboop = function (input) {
     return "Please enter a positive number.";
   }
   var number1 = [];
-  var number2 = [];
 
 
   for (var i = 1; i <= input; i++){
     number1.push(i);
   };
+
+  //replaces all divisibles of 3 with "I'm sorry, Dave I'm afraid I can't do that."
  for (var i = 1; i < number1.length; i++){
    if(number1[i] % 3 === 0){
      number1[i] = ("I'm sorry, Dave I'm afraid I can't do that.");
    }
  }
-
-
+//replaces all numbers that end with 1 or 0 with beep or boop
 for (var i = 1; i < number1.length; i++){
   if(number1[i] % 10 === 0){
     number1[i] = ("Beep!");
@@ -28,32 +28,9 @@ for (var i = 1; i < number1.length; i++){
   }
 }
 
- //for (var i = 0; i < number2.length; i++){
-//   if(number2[i] )
- //}
+// replaces all numbers that begin with 1 or 0 with beep or Boop
 
 
-  /*if number1 % 3 = 0 {
-    number1.remove();
-}
-
-for (var i = 0; i <= input; i++){
-  number1.push(i);
-};
-
-*/
-
-  /*if (input = 3){
-    number1.push('Beep!')
-  }
-  if (input = 4){
-    number1.push('Boop!')
-  }
-  for (var i = 0; i <= input; i++){
-    if(input % 3 = 0){
-      number1.push(i)
-    }
-  }*/
 
 var total = number1;
 
