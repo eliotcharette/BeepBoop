486f3d8b499a1a0ac551dd6ed4d50aa866b9d785
eliotcharette <eliotcharette@gmail.com>
2018-06-08 15:00:11 -0700

    Clean lines

--- js/scripts.js
+++ js/scripts.js
@@ -7,52 +7,56 @@ var beepboop = function (input) {
 
   for (var i = 0; i <= input; i++){
     number1.push(i);
-  };
-  //replaces all divisibles of 3 with "I'm sorry, Dave I'm afraid I can't do that."
- for (var i = 1; i < number1.length; i++){
-   if(number1[i] % 3 === 0){
-     number1[i] = ("I'm sorry, Dave I'm afraid I can't do that.");
-   }
- }
-//replaces all numbers that end with 1 or 0 with beep or boop
-for (var i = 1; i < number1.length; i++){
-  if(number1[i] % 10 === 1){
-    number1[i] = ("Boop!");
   }
+  //replaces all divisibles of 3 with "I'm sorry, Dave I'm afraid I can't do that."
+  for (var i = 1; i < number1.length; i++){
+    if(number1[i] % 3 === 0){
+      number1[i] = ("I'm sorry, Dave I'm afraid I can't do that.");
+    }
+  }
+  //replaces all numbers that end with 1 or 0 with beep or boop
+  for (var i = 1; i < number1.length; i++){
+    if(number1[i] % 10 === 1){
+      number1[i] = ("Boop!");
+    }
+  }
+  for (var i = 0; i < number1.length; i++){
+    if(number1[i] % 10 === 0){
+      number1[i] = ("Beep!");
+    }
+  }
+  // replaces all numbers that begin with 1 with Boop
+  /*for (var i = 1; i < number1.lenth; i++){
+  if (number1[i] = 1){
+  number1[i] = ("Boop!")
+}
 }
 for (var i = 0; i < number1.length; i++){
-  if(number1[i] % 10 === 0){
-    number1[i] = ("Beep!");
-  }
+if(number1[i] < 19 && number1 > 10){
+number1[i] = ("Boop!");
 }
-
-
-// replaces all numbers that begin with 1 with Boop
-/*for (var i = 1; i < number1.lenth; i++){
-  if (number1[i] = 1){
-    number1[i] = ("Boop!")
-  }
-}*/
-/*for (var i = 1; i < number1.length; i++){
-  if(num.indexOf(number1[i]) > -1){
-    var slice_point = i;
-  }
 }
 */
 /*for (var i = 1; i < number1.length; i++){
-  if(num.indexOf(number1[i]) > -1){
-    number1[i] = ("Boop!")
-  }
+if(num.indexOf(number1[i]) > -1){
+var slice_point = i;
+}
+}
+*/
+/*for (var i = 1; i < number1.length; i++){
+if(num.indexOf(number1[i]) > -1){
+number1[i] = ("Boop!")
+}
 }*/
 /*for (var i = 1; i < number1.length; i++){
-  if(number1.indexOf("1") = 0){
-    number1[i] = ("Boop!")
-  }
+if(number1.indexOf("1") = 0){
+number1[i] = ("Boop!")
+}
 }*/
 /*for (var i = 1; i < number1.length; i++){
-  if(number1[i].startsWith(1)){
-    number1[i] = ("Boop!")
-  }
+if(number1[i].startsWith(1)){
+number1[i] = ("Boop!")
+}
 }*/
 var total = number1;
 
